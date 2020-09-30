c3c73ab895516383a9ccf30f0d0c4f3e3b5af6f4
Spencer Roach <sroach@matsolutionsllc.com>
2020-09-29 23:43:51 -0400

    remove unused controller actions

--- KingsmenSmartAC.API/Controllers/DeviceController.cs
+++ KingsmenSmartAC.API/Controllers/DeviceController.cs
@@ -56,31 +56,5 @@ namespace KingsmenSmartAC.API.Controllers
         {
             return await _context.Devices.ToListAsync();
         }
-
-
-        // GET api/<DeviceController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<DeviceController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<DeviceController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<DeviceController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
